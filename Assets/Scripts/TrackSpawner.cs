a391d7c794cc2abedc0070274b374a85e3e72a79
Belen Garcia Quesada <bgarque830@g.educaand.es>
2026-01-27 12:36:19 +0100

    Control de líneas ocupada y spawn de coleccionables no funcionales
    Como cuando creo objetos lo hago siempre en y = 0.3f, se clipean con el suelo. Obviamente falta un colisionador trigger y que actualice el puntaje.

--- Assets/Scripts/TrackSpawner.cs
+++ Assets/Scripts/TrackSpawner.cs
@@ -4,12 +4,14 @@ using System.Collections.Generic;
 public class TrackSpawner : MonoBehaviour
 {
     public GameObject trackPrefab;
-    public GameObject[] obstaclePrefab;
+    public GameObject[] obstaclePrefabs;
+    public GameObject[] collectablePrefabs;
 
     public int initialPieces = 5;
     public float pieceLength = 5f;
     public float speed = 2f;
     public float obstacleChance = 0.5f;
+    public float collectableChance = 0.5f;
 
     public float speedIncreaseInterval = 5f;
     public float speedIncreaseAmount = 0.2f;
@@ -104,24 +106,52 @@ public class TrackSpawner : MonoBehaviour
 
         piece.transform.position = new Vector3(0, -0.3f, spawnZ);
 
+        // [!] Lo tengo que declarar aquí porque para cada track piece se reinicia
+        // Carriles (para comprobar si están ocupados o no)
+        bool[] laneOccupied = new bool[3];
+
         // Posible obstáculo
         if (Random.value < obstacleChance)
-        {
-            SpawnObstacle(piece.transform);
-        }
+            SpawnObject(piece.transform, obstaclePrefabs, laneOccupied);
+        
+        // Posible coleccionable
+        if (Random.value < collectableChance)
+            SpawnObject(piece.transform, collectablePrefabs, laneOccupied);
 
         trackQueue.Enqueue(piece);
     }
 
-
-    void SpawnObstacle(Transform parent)
+    void SpawnObject(Transform parent, GameObject[] objectPrefab, bool[] laneOccupied)
     {
-        float[] lanes = { -0.4f, 0f, 0.4f };
-        float x = lanes[Random.Range(0, lanes.Length)];
+        // Lista de lanes disponibles (list porque no va a saber el tamaño hasta que llegue)
+        List<int> freeLanes = new List<int>();
 
-        GameObject obstacle = Instantiate(obstaclePrefab[Random.Range(0, obstaclePrefab.Length)]);
-        obstacle.transform.SetParent(parent);
-        obstacle.transform.localPosition = new Vector3(x, 0.3f, 0);
+        // Se recorre el array de lanes pasado. ¿No está ocupada la posición? A freeLanes
+        for (int i = 0; i < laneOccupied.Length; i++)
+        {
+            if (!laneOccupied[i])
+            {
+                freeLanes.Add(i);
+            }
+        }
+
+        // Si no queda ningún lane libre, fuera
+        if (freeLanes.Count == 0)
+            return;
+
+        // Hay lanes libres, elegir uno al azar
+        int laneIndex = freeLanes[Random.Range(0, freeLanes.Count)];
+
+        float[] lanePositions = { -0.4f, 0f, 0.4f };
+        float x = lanePositions[laneIndex]; // Ya no coge una random de los lanes, solo busca EN LOS QUE HAY LIBRES
+
+        // Instanciar el objeto como antes...
+        GameObject objectSpawned = Instantiate(objectPrefab[Random.Range(0, objectPrefab.Length)]);
+        objectSpawned.transform.SetParent(parent);
+        objectSpawned.transform.localPosition = new Vector3(x, 0.3f, 0);
+
+        // Marcar lane como ocupado en la posición correspondiente
+        laneOccupied[laneIndex] = true;
     }
 
     void RemovePiece()
